90bc2f2ff1277e65fd3d8fcc6cacb2c8fbd0a2e1
liralgazi <liralgazi@gmail.com>
2024-03-23 13:14:24 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,31 +5,42 @@ import http from 'http';
 import { Server } from 'socket.io';
 import { saveMessage, getAllMessages, getPageOfMessages } from './config/db'; 
 import dotenv from 'dotenv';
+import path from 'path'; // Import path module for handling file paths
+
 const app = express();
 dotenv.config();
 const server = http.createServer(app);
 const io = new Server(server, {});
+
 app.use(express.json());
 app.use('/api', routes);
 
 const corsOptions = {
     origin: '*', 
     allowedHeaders: ['Content-Type'], 
-  };
+};
 
 app.use(cors(corsOptions));
+
+// Serve the React frontend as static files
+const frontendBuildPath = path.join(__dirname, '..', 'frontend', 'build');
+app.use(express.static(frontendBuildPath));
+
+// Serve the React app for any routes not prefixed with /api
+app.get('*', (req, res) => {
+    res.sendFile(path.join(frontendBuildPath, 'index.html'));
+});
+
 const PORT = process.env.PORT || 3002;
 
 server.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 });
 
-
 io.on('connection', async (socket) => {
     console.log('A user connected');
 
     socket.on('message', async (message) => {
-        //console.log("Message received: ", message);
         await saveMessage(message);
         io.emit('message', message);
     });
@@ -38,7 +49,6 @@ io.on('connection', async (socket) => {
         console.log('A user disconnected');
     });
 });
-
 
 app.get('/api/messages', async (req, res) => { 
     let messages;
@@ -52,5 +62,4 @@ app.get('/api/messages', async (req, res) => {
         messages = await getAllMessages();
     }
     res.json(messages);
-  });
-  
+});
